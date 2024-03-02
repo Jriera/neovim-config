6b7b1c638fc06fdc4555fc4baa6f8fefc4d7afad
Jordi Riera Alentorn <jordi.riera.sge@mango.com>
2024-03-03 00:55:19 +0100

    feat: add key remaps for trouble plugin

--- lua/enderVim/plugins/trouble.lua
+++ lua/enderVim/plugins/trouble.lua
@@ -1,2 +1,14 @@
 return {
+    
+    {
+    "folke/trouble.nvim",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    opts = {
+            icons = true
+    },
+    config = function()
+        vim.keymap.set("n","<leader>tt",function() require("trouble").toggle() end)
+        vim.keymap.set("n", "<leader>tn", function() require("trouble").next({skip_groups=true, jump= true}) end)
+        end
+    },
 }
