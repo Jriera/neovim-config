18a5d363a18360e2b59bf2d314ffe31876d8addc
Jordi Riera Alentorn <jordi.riera.sge@mango.com>
2024-03-03 01:34:06 +0100

    feat: add a remap to swap lines

--- lua/enderVim/remap.lua
+++ lua/enderVim/remap.lua
@@ -2,4 +2,6 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end)
 vim.keymap.set("n", "gg", vim.cmd.LazyGit)
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-3<CR>gv=gv")
         
