74caa2c5b5453db3f2a1ae66174f794cc6af98c2
Jordi Riera Alentorn <jordi.riera.sge@mango.com>
2024-03-16 18:56:38 +0100

    adjusting some opts

--- lua/enderVim/config/init.lua
+++ lua/enderVim/config/init.lua
@@ -9,7 +9,7 @@ end
 -------
 
 local indent = 4
-vim.o.formatoptions = "jcroqlnt"
+vim.o.formatoptions = "jcqnt"
 vim.o.shortmess = "filnxtToOFWIcC"
 vim.opt.breakindent = true
 vim.opt.cmdheight = 1
@@ -21,7 +21,7 @@ vim.opt.expandtab = true
 vim.opt.hidden = true
 vim.opt.hlsearch = false
 vim.opt.ignorecase = true
-vim.opt.inccommand = "nosplit"
+vim.opt.inccommand = "split" -- show changes incrementally in a split window specially useful for substitutions
 vim.opt.joinspaces = false
 vim.opt.laststatus = 0
 vim.opt.list = true
@@ -35,16 +35,20 @@ vim.opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize" }
 vim.opt.shiftround = true
 vim.opt.shiftwidth = indent
 vim.opt.showmode = false
-vim.opt.sidescrolloff = 8
+vim.opt.sidescrolloff = 10
 vim.opt.signcolumn = "yes"
 vim.opt.smartcase = true
 vim.opt.smartindent = true
 vim.opt.splitbelow = true
 vim.opt.splitkeep = "screen"
 vim.opt.splitright = true
-vim.opt.tabstop = indent
+vim.opt.tabstop = 4 -- number of spaces that a <Tab> in the file counts for
+vim.opt.shiftwidth = 4 -- number of spaces that a <Tab> counts for when using the > or < commands
+vim.opt.virtualedit = "block" -- allow cursor to move anywhere in visual block mode 
 vim.opt.termguicolors = true
 vim.opt.timeoutlen = 300
 vim.opt.undofile = true
 vim.opt.updatetime = 200
-opt('o', 'termguicolors', true)
+vim.opt.wrap = false
+-- opt('o', 'termguicolors', true)
+-- vim.opt.clipboard = "unnamedplus" this synchronizes the system clipboard with the buffer clipboard
