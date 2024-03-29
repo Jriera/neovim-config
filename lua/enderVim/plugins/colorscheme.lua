b59cfc379feff42854f6c404c577c7c336a98eb2
Jordi Riera Alentorn <jordiriera81@gmail.com>
2024-03-29 18:59:12 +0100

    fix:add color scheme to cmp plugin

--- lua/enderVim/plugins/colorscheme.lua
+++ lua/enderVim/plugins/colorscheme.lua
@@ -24,6 +24,47 @@ return {
                 hi["@keyword"] = { fg = "#b701b4", style = { italic = true } }
                 hi.String = { fg = "#ecc48d", style = { italic = true } }
                 hi["lsp.type.decorator"] = { fg = "#82AAFF", style = { italic = true } }
+                -- floating menu styles cmp
+                hi["PmenuSel"] = { bg = "#282C34", fg = "NONE" }
+                hi["Pmenu"] = { fg = "#C5CDD9", bg = "#22252A" }
+
+                hi["CmpItemAbbrDeprecated"] = { fg = "#7E8294", bg = "NONE", strikethrough = true }
+                hi["CmpItemAbbrMatch"] = { fg = "#82AAFF", bg = "NONE", bold = true }
+                hi["CmpItemAbbrMatchFuzzy"] = { fg = "#82AAFF", bg = "NONE", bold = true }
+                hi["CmpItemMenu"] = { fg = "#C792EA", bg = "NONE", italic = true }
+
+                hi["CmpItemKindField"] = { fg = "#EED8DA", bg = "#B5585F" }
+                hi["CmpItemKindProperty"] = { fg = "#EED8DA", bg = "#B5585F" }
+                hi["CmpItemKindEvent"] = { fg = "#EED8DA", bg = "#B5585F" }
+
+                hi["CmpItemKindText"] = { fg = "#C3E88D", bg = "#9FBD73" }
+                hi["CmpItemKindEnum"] = { fg = "#C3E88D", bg = "#9FBD73" }
+                hi["CmpItemKindKeyword"] = { fg = "#C3E88D", bg = "#9FBD73" }
+
+                hi["CmpItemKindConstant"] = { fg = "#FFE082", bg = "#D4BB6C" }
+                hi["CmpItemKindConstructor"] = { fg = "#FFE082", bg = "#D4BB6C" }
+                hi["CmpItemKindReference"] = { fg = "#FFE082", bg = "#D4BB6C" }
+
+                hi["CmpItemKindFunction"] = { fg = "#EADFF0", bg = "#A377BF" }
+                hi["CmpItemKindStruct"] = { fg = "#EADFF0", bg = "#A377BF" }
+                hi["CmpItemKindClass"] = { fg = "#EADFF0", bg = "#A377BF" }
+                hi["CmpItemKindModule"] = { fg = "#EADFF0", bg = "#A377BF" }
+                hi["CmpItemKindOperator"] = { fg = "#EADFF0", bg = "#A377BF" }
+
+                hi["CmpItemKindVariable"] = { fg = "#C5CDD9", bg = "#7E8294" }
+                hi["CmpItemKindFile"] = { fg = "#C5CDD9", bg = "#7E8294" }
+
+                hi["CmpItemKindUnit"] = { fg = "#F5EBD9", bg = "#D4A959" }
+                hi["CmpItemKindSnippet"] = { fg = "#F5EBD9", bg = "#D4A959" }
+                hi["CmpItemKindFolder"] = { fg = "#F5EBD9", bg = "#D4A959" }
+
+                hi["CmpItemKindMethod"] = { fg = "#DDE5F5", bg = "#6C8ED4" }
+                hi["CmpItemKindValue"] = { fg = "#DDE5F5", bg = "#6C8ED4" }
+                hi["CmpItemKindEnumMember"] = { fg = "#DDE5F5", bg = "#6C8ED4" }
+
+                hi["CmpItemKindInterface"] = { fg = "#D8EEEB", bg = "#58B5A8" }
+                hi["CmpItemKindColor"] = { fg = "#D8EEEB", bg = "#58B5A8" }
+                hi["CmpItemKindTypeParameter"] = { fg = "#D8EEEB", bg = "#58B5A8" }
             end
         })
 
@@ -31,6 +72,11 @@ return {
         vim.cmd.colorscheme("tokyonight")
     end
 }
+
+-- Customization for Pmenu
+
+
+
 -- alternate colorscheme
 --[[{
         "tiagovla/tokyodark.nvim",
